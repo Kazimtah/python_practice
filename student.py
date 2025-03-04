88de7a69115a1341d909e0fee044691ca952a93f
Kazimtha <kazimhamid02@gmail.com>
2025-03-04 14:16:34 +0100

    Adding a function to add up the number of student

--- student.py
+++ student.py
@@ -9,13 +9,21 @@ class Student():
 
 
 student1 = Student("Mohamad Kazim", 31)
-print(student1.name, student1.age, student1.class_year)
+#print(student1.name, student1.age, student1.class_year)
 student2 = Student("Patrick", 35, )
-print(student2.name, student2.age, student2.class_year)
+#print(student2.name, student2.age, student2.class_year)
 
 student3 = Student("Alyas", 32)
-print(student3.name, student3.age)
+#print(student3.name, student3.age)
+student4 = Student("Zaman", 24)
 
 
-print("***********************")
-print(Student.num_student)
+#print("***********************")
+#print(Student.num_student)
+
+print(f"My graduated student of {Student.class_year} has number of {Student.num_student}")
+
+print(student1.name, student4.age)
+print(student2.name, student4.age)
+print(student3.name, student4.age)
+print(student4.name, student4.age)
