4ceeeeffc3af183fd47b482cf50640d9115257f3
Kazimtha <kazimhamid02@gmail.com>
2025-03-12 19:43:09 +0100

    adding new feather

--- list_extend.py
+++ list_extend.py
@@ -1,4 +1,13 @@
-a = [ 1,2,3,4]
-b = [3,4,5,6]
-c = a + b
-print(c)
+def add(*args):
+    sum = 0
+    for i in range (len(args)):
+        sum += args[i]
+    return sum 
+
+print(add(3,4,5,6,7))
+
+def printAllvarialbe(**args):
+    for x in args:
+        print("Variable name is ", x, "And value is :", args[x])
+
+print(printAllvarialbe(a= 3, b= "B", c="CC"))
